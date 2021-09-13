a3ffa123009dd670a573807a75172eda5fa3ae46
Kanke <sugarkanke@yahoo.com>
2021-09-13 14:27:06 +0100

    adding footer component

---
+++ src/components/Sidebar/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+
+    return (
+        <div>
+            <footer className="small text">
+                <span> About. Help . Press . API . Jobs . Privacy . Terms . Locations . Top . Accounts . Hashtags . Language </span>
+                <p>© 2021 BOOLGRAM FROM BOOLEAN</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,14 +2,14 @@ import React from 'react';
 import User from "./User";
 import './Sidebar.css';
 import Suggestions from "./Suggestions";
+import Footer from "./Footer";
 
 function Sidebar() {
 return (
     <div className="two">
         <User />
         <Suggestions />
-        {/*<User username={username} fullName={fullName} />*/}
-        {/*<Suggestions userId={userId} following={following} loggedInUserDocId={docId} />*/}
+        <Footer />
     </div>
 );
 
